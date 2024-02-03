63e54415849012de1db31d2bcb9fea88857bcfbf
neonz <neonzdeveloper@gmail.com>
2024-02-03 13:10:38 +0100

    Simple array created

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,5 +1,10 @@
-public class Main {
+public class Arrays {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {1, 2, 3, 4, 5, 6};
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("nums[" + i + "] = " + nums[i]);
+        }
+
     }
 }
